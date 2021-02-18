b21759579f2a3b7ce720c0da2346b9cefa098a9a
stephanpeck <62480543+stephanpeck@users.noreply.github.com>
2021-02-18 02:56:23 -0700

    Add project files.

---
+++ Block.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace InfoCoin
+{
+    class Block
+    {
+        public int Index { get; set; }
+        public string PreviousHash { get; set; }
+        public string TimeStamp { get; set; }
+        //public string Data { get; set; }
+        public string Hash { get; set; }
+        public int Nonce { get; set; }
+        public List<Transaction> Transactions { get; set; }
+
+
+        //previousHash set to "" because the Genesis block will not have a previous hash
+        public Block(int index, string timestamp, List<Transaction> transactions, string previousHash = "")
+        {
+            this.Index = index;
+            this.TimeStamp = timestamp;
+            this.Transactions = transactions;
+            this.PreviousHash = previousHash;
+            this.Hash = CalculateHash();
+            this.Nonce = 0;
+        }
+
+        public string CalculateHash()
+        {
+            string blockData = this.Index + this.PreviousHash + this.TimeStamp + this.Transactions.ToString() + this.Nonce;
+            byte[] blockBytes = Encoding.ASCII.GetBytes(blockData);
+            byte[] hashBytes = SHA256.Create().ComputeHash(blockBytes);
+            //converts back to a more readable format
+            return BitConverter.ToString(hashBytes).Replace("-", "");
+        }
+
+        public void Mine(int difficulty)
+        {
+            
+            while (this.Hash.Substring(0,difficulty) != new String('0', difficulty))
+            {
+                this.Nonce++;
+                this.Hash = this.CalculateHash();
+                //Uncomment if you want to see all the hash attempts
+                //Console.WriteLine("Mining: " + this.Hash);
+            }
+
+            Console.WriteLine("Block has been mined: " + this.Hash);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace InfoCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrivateKey key1 = new PrivateKey();
+            PublicKey wallet1 = key1.publicKey();
+
+            PrivateKey key2 = new PrivateKey();
+            PublicKey wallet2 = key2.publicKey();
+            
+            //parameters are difficulty and reward for mining
+            Blockchain infocoin = new Blockchain(3, 100);
+
+
+            Console.WriteLine("Start the Miner.");
+            infocoin.MinePendingTransactions(wallet1);
+            Console.WriteLine("\n Balance of wallet1 is $:" + infocoin.GetBalanceOfWallet(wallet1).ToString());
+
+
+
+
+            //OLD Leftover
+            //infocoin.AddBlock(new Block(1, DateTime.Now.ToString("yyyyMMddHHmmssffff"), "amount: 50"));
+            //infocoin.AddBlock(new Block(2, DateTime.Now.ToString("yyyyMMddHHmmssffff"), "amount: 200"));
+
+
+
+            Transaction tx1 = new Transaction(wallet1, wallet2, 10);
+            tx1.SignTransaction(key1);
+            infocoin.addPendingTransaction(tx1);
+            Console.WriteLine("Start the Miner.");
+            infocoin.MinePendingTransactions(wallet2);
+            Console.WriteLine("\n Balance of wallet1 is $:" + infocoin.GetBalanceOfWallet(wallet1).ToString());
+            Console.WriteLine("\n Balance of wallet2 is $:" + infocoin.GetBalanceOfWallet(wallet2).ToString());
+
+
+
+            string blockJSON = JsonConvert.SerializeObject(infocoin, Formatting.Indented);
+
+            Console.WriteLine(blockJSON);
+
+
+            //infocoin.GetLatestBlock().PreviousHash = "12345";
+
+
+            if (infocoin.IsChainValid())
+            {
+                Console.WriteLine("Blockchain is Valid!");
+            }
+            else
+            {
+                Console.WriteLine("Blockchain is NOT Valid!");
+            }
+
+        }
+    }
+
+    
+
+    
+}
